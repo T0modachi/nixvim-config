d2d1a7486beb6aed568246245f66cd4d5d49ecd1
T0modachi <jvergarava@gmail.com>
2024-09-09 10:42:35 +0200

    add keymaps for neotest

--- config/plug/lsp/neotest.nix
+++ config/plug/lsp/neotest.nix
@@ -21,4 +21,97 @@
       };
     };
   };
+
+  keymaps = [
+    {
+      mode = ["n"];
+      key = "<leader>tt";
+      action = "<cmd>lua require('neotest').run.run(vim.fn.expand('%')<cr>";
+      options = {
+        silent = true;
+        desc = "Run File";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tT";
+      action = "<cmd>lua require('neotest').run.run(vim.uv.cwd()<cr>";
+      options = {
+        silent = true;
+        desc = "Run All Test Files";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tr";
+      action = "<cmd>lua require('neotest').run.run()<cr>";
+      options = {
+        silent = true;
+        desc = "Run Nearest";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tl";
+      action = "<cmd>lua require('neotest').run.run_last()<cr>";
+      options = {
+        silent = true;
+        desc = "Run Last";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>ts";
+      action = "<cmd>lua require('neotest').summary.toggle()<cr>";
+      options = {
+        silent = true;
+        desc = "Toggle Summary";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>to";
+      action = "<cmd>lua require('neotest').output.open({ enter = true, auto_close = true })<cr>";
+      options = {
+        silent = true;
+        desc = "Show Output";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tO";
+      action = "<cmd>lua require('neotest').output_panel.toggle()<cr>";
+      options = {
+        silent = true;
+        desc = "Toggle Output Panel";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tS";
+      action = "<cmd>lua require('neotest').run.stop()<cr>";
+      options = {
+        silent = true;
+        desc = "Stop";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>tw";
+      action = "<cmd>lua require('neotest').watch.toggle(vim.fn.expand('%'))<cr>";
+      options = {
+        silent = true;
+        desc = "Toggle Watch";
+      };
+    }
+    {
+      mode = ["n"];
+      key = "<leader>td";
+      action = "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>";
+      options = {
+        silent = true;
+        desc = "Debug Nearest";
+      };
+    }
+  ];
 }
