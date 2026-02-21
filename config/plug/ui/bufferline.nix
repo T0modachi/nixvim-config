f2f05d83f22fc0374f4f5387b89fb413906d2c4e
T0modachi <jvergarava@gmail.com>
2026-02-20 21:41:54 -0300

    update bufferline option

--- config/plug/ui/bufferline.nix
+++ config/plug/ui/bufferline.nix
@@ -11,7 +11,9 @@ in
     bufferline = {
       enable = true;
       settings = {
-        separatorStyle = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+        options = {
+          separatorStyle = "thick"; # “slant”, “padded_slant”, “slope”, “padded_slope”, “thick”, “thin”
+        };
         highlights = lib.mkIf config.colorschemes.base16.enable {
           fill = {
             fg = colors.base00;
